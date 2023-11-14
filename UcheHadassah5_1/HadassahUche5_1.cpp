bcbb27060f14cc9eb6d080add4714164679f9f94
Hadassah Uche <uhadassah1@gmail.com>
2023-11-14 14:22:54 +0200

    Cleaned up the code and added comments. The code is complete and correct

--- UcheHadassah5_1/HadassahUche5_1.cpp
+++ UcheHadassah5_1/HadassahUche5_1.cpp
@@ -1,44 +1,34 @@
-/*Write a program that reads the grades (integers) of 4 exams per students in a class 
-of 3 students, and outputs the following statistics: a) the lowest grade b) the highest 
-grade and c) the average grade of each student. You should also output the table of 
-grades entered. 
-Example output:
-Student 0 77 68 86 73
-Student 1 96 87 89 78
-Student 2 70 90 86 81
-Lowest grade: 68
-Highest grade: 96
-The average grade of student 0 is 76.00
-The average grade of student 1 is 87.50
-The average grade of student 1 is 81.75*/
+/*Name: Uche Hadassah
+This program reads the grades of 4 subjects for 3 students and outputs the lowest grade, highest grade and 
+the average grade for each student*/
 #include<iostream>
 using namespace std;
 int main()
 {
-	int grade[3][4] = { 77,68,86,73,96,87,89,78,70,90,86,81 };
-	int lowest = grade[0][0];
-	int highest = grade[0][0];
-	double sum[] = { 0.0,0.0,0.0 };
+	int grade[3][4] = { 77,68,86,73,96,87,89,78,70,90,86,81 };//hardcoded the grades
+	int lowest = grade[0][0];//Set to the first element in the array
+	int highest = grade[0][0];//Set to the first element in the array
+	double sum[] = { 0.0,0.0,0.0 };//Created a 1-D array to store the sum of grades for each student and initialized it all to 0.0
 	for (int i = 0; i < 3; i++)
 	{
 		cout << "\nStudent " << i << " ";
 		for (int j = 0; j < 4; j++)
 		{
-			sum[i] += grade[i][j];
+			sum[i] += grade[i][j];//adds to the sum for student i
 			cout << grade[i][j] << " ";
 			if (grade[i][j] < lowest)
 			{
-				lowest = grade[i][j];
+				lowest = grade[i][j];// Sets the lowest grade
 			}
 			else if (grade[i][j] > highest)
 			{
-				highest = grade[i][j];
+				highest = grade[i][j];//Sets the highest grade
 			}
 		}
 	}
 	cout << "\nLowest Grade: " << lowest;
 	cout << "\nHighest Grade: " << highest;
-	for(int k = 0;k<3;k++)
+	for(int k = 0;k<3;k++)//Outputs the average for each student
 	{
 		double avg = sum[k] / 4;
 		cout << "\nThe average grade of student " << k << " is " << avg;
